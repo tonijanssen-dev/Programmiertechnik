d5d26488bf90d5d2e90e1b45a4fca50cf11ed58a
Toni Janßen <tonijanssen1992@hotmail.com>
2025-09-01 22:41:56 +0200

    Übung

---
+++ Main.java
@@ -0,0 +1,138 @@
+// public class Main{ //Main.java
+//    public static void main(String[] args) {
+//        /*The code below will print the words Hello World to the screen*/
+//       System.out.println("Hello World!");
+//    }   
+//}
+//
+
+// single-line comments (short comments)
+/* multi-line comments (longer comments*/
+
+/* Variable sind in Java Containers um Daten Werte zu speichern.
+   Variable are containers to store data values.
+   z.B. verschiedene Java Variablen /different Java variables
+        String: text 
+        int: integers (ganze zahlen/whole numbers) 
+        float: floating point numbers (with decimals/ Gleitkommazahlen mit dezimalstelle)
+        char: single charakter (einzelne Zeichen)
+        boolean: true or false
+ */
+
+/*public class Main {
+    public static void main(String[] args){
+        String name = "John";
+        System.out.println(name);
+    }
+}
+*/
+
+/*public class Main {
+    public static void main(String[] args){
+        int myNum = 15; //integer variable deklariert als myNum und auf 15 gesetzt
+        System.out.println(myNum);
+    }
+}
+*/
+
+/*public class Main{
+    public static void main(String[] args){
+        String name = "Toni";
+        System.out.println("Hello " + name); //Text mit Variable kombinieren und ausgeben lassen
+    }
+}
+*/
+
+/*public class Main{
+    public static void main(String[] args){
+        String firstName = "Toni"; //Variable 1 firstName = benannt "Toni"
+        String lastName = " Janssen"; // Variable 2 firstName = benannt " Janssen"
+        String fullName = firstName + lastName; // Variable 3 fullName = Variable 1 + Variable 2 
+        System.out.println(fullName); // gibt die Variable 3 aus welche firstName lastName kombiniert ist
+    }
+}
+*/
+
+/*public class Main{
+    public static void main(String[] args){
+        int x = 5, y = 5, z = 5; // mehrere Variable sind hier deklariert und initialisiert x = 5(initialisiert), y= 5(initialisiert), z = 5(initialisiert) und durch ein KOMMA getrennt
+        System.out.println(x + y + z);
+    }
+}
+*/
+
+/*public class Main{
+    public static  void main(String[] args){
+    int x, y, z; // int wurde mit Variable x,y,z deklariert
+    x = y = z = 50; // alle Variablen wurde ein Numerische Wert zugewiesen (INITIALISIERT)
+    System.out.println(x + y + x); //sollte 150 ergeben denn, x(50) + y(50) + x(50) =150
+    }
+}
+*/
+
+
+
+
+public class Main{
+    public static void main(String[] args){
+        // Student data / Studenten Daten
+        String studentName = "John Doe";
+        int studentID = 15;
+        int studentAge = 23;
+        float studentFee = 75.25f;
+        char studentGrade = 'B';
+
+        // Print variables / Variablen ausgeben lassen
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Student Age: " + studentAge);
+        System.out.println("Student Fee: " + studentFee);
+        System.out.println("Student Grade: " + studentGrade);     
+    }
+}
+
+
+/*public class Main{
+    public static void main(String[] args){
+        // Create integer variables
+        int length = 4;
+        int width = 6;
+        int area;
+
+        // Calculate the area of a rectangle / Berechne die Fläche eines Dreiecks
+        area = length * width;
+
+        //Print variables 
+        System.out.println("Length is: " + length);
+        System.out.println("Width is: " + width);
+        System.out.println("Rectangle area is: " + area);
+
+    }
+}
+    */
+
+
+
+//public class Main{
+//    public static void main(String[] args){
+//
+//    }
+//}
+
+//public class Main{
+//    public static void main(String[] args){
+//
+//    }
+//}
+
+//public class Main{
+//    public static void main(String[] args){
+//
+//    }
+//}
+
+//public class Main{
+//    public static void main(String[] args){
+//
+//    }
+//}
